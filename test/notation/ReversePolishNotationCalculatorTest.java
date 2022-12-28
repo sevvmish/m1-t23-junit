d5bed126744d0f4503056b00c7ed996f74b6ed99
Krivtsov <sevvmish@gmail.com>
2022-12-28 13:38:47 +0300

    тестирование ReversePolishNotationCalculator

--- test/notation/ReversePolishNotationCalculatorTest.java
+++ test/notation/ReversePolishNotationCalculatorTest.java
@@ -1,11 +1,55 @@
 package notation;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ReversePolishNotationCalculatorTest {
+    protected ReversePolishNotationCalculator notation;
+    @BeforeEach
+    public void beforeEach() {
+        notation = new ReversePolishNotationCalculator();
+    }
 
     @Test
-    public void shouldCalculateAddition() {
+    @DisplayName("Тест на сложение")
+    public void additionTest() {
+        String data = "1 3 +";
+        int result = notation.calculatePolishNotation(data);
+        assertEquals(4, result);
+    }
 
+    @Test
+    @DisplayName("Тест на вычитание")
+    public void subtractionTest() {
+        String data = "5 7 -";
+        int result = notation.calculatePolishNotation(data);
+        assertEquals(-2, result);
+    }
+
+    @Test
+    @DisplayName("Тест на умножение")
+    public void multiplicationTest() {
+        String data = "3 7 *";
+        int result = notation.calculatePolishNotation(data);
+        assertEquals(21, result);
+    }
+
+    @Test
+    @DisplayName("Тест на отрицательные числа")
+    public void negativeNumberTest() {
+        String data = "-3 2 -1 * -";
+        int result = notation.calculatePolishNotation(data);
+        assertEquals(-1, result);
+    }
+
+    @Test
+    @DisplayName("Тест на пробелы")
+    public void spacesTest() {
+        String data = "-4  -5     7 - +";
+        int result = notation.calculatePolishNotation(data);
+        assertEquals(-16, result);
     }
 }
